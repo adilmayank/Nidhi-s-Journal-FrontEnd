cec6849c70ec2411e412bfafa2359b5bfb61a16d
adilmayank <adilmayank5894@gmail.com>
2023-05-12 22:17:51 +0530

    added Download Button Component

---
+++ src/components/Buttons/DownloadAllJournals.js
@@ -0,0 +1,44 @@
+import { Button, Stack } from '@mui/material'
+import { saveAs } from 'file-saver'
+import { FileDownload } from '@mui/icons-material'
+import axios from 'axios'
+
+const DownloadAllJournalsButton = () => {
+  const bearerToken = localStorage.getItem('token')
+  const authenticationHeader = {
+    headers: { Authentication: `Bearer ${bearerToken}` },
+  }
+
+  const handleClick = async () => {
+    const response = await axios.get(
+      'http://localhost:5000/api/v1/dev/journals/download',
+      authenticationHeader
+    )
+
+    const text = response.data
+    const filename = `All_Journals_${new Date(
+      Date.now()
+    ).toLocaleDateString()}.txt`
+    const blob = new Blob([text], { type: 'text/plain;charset=utf-8' })
+    saveAs(blob, filename)
+  }
+
+  return (
+    <Stack display="flex" direction={'row'} justifyContent={'flex-end'}>
+      <Button
+        variant="contained"
+        sx={{
+          color: '#fff',
+          width: '150px',
+          fontSize: '10px',
+        }}
+        endIcon={<FileDownload sx={{ scale: '0.8' }} />}
+        onClick={handleClick}
+      >
+        Download All
+      </Button>
+    </Stack>
+  )
+}
+
+export default DownloadAllJournalsButton
